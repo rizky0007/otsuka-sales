e9b8bca22fb4ad5fe15e7f8baa744b4915822e57
rizkysaputra007 <apalaj00@gmail.com>
2026-08-30 22:18:18 +0700

    Update halaman value

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  const supabaseUrl =
-    process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
   const supabaseKey =
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
